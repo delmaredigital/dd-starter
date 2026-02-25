9ee815d7382d5705d9204dd8e91e309fdfb8dc68
allandelmare <8284237+allandelmare@users.noreply.github.com>
2026-02-25 09:17:53 -0500

    SEO

--- tests/e2e/frontend.e2e.spec.ts
+++ tests/e2e/frontend.e2e.spec.ts
@@ -4,10 +4,10 @@ test.describe('Frontend', () => {
   test('can go on homepage', async ({ page }) => {
     await page.goto('http://localhost:3000')
 
-    await expect(page).toHaveTitle(/Payload Website Template/)
+    await expect(page).toHaveTitle(/DD-Starter Template/)
 
     const heading = page.locator('h1').first()
 
-    await expect(heading).toHaveText('Payload Website Template')
+    await expect(heading).toHaveText('DD-Starter Template')
   })
 })
