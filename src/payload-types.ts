c6706c4d0f035b4ba248cdd6a70e20c2b7c4b59d
allandelmare <8284237+allandelmare@users.noreply.github.com>
2026-03-05 20:31:39 -0500

    update packages + better auth plugin 0.5.6

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -126,6 +126,9 @@ export interface Config {
     footer: FooterSelect<false> | FooterSelect<true>;
   };
   locale: null;
+  widgets: {
+    collections: CollectionsWidget;
+  };
   user: User;
   jobs: {
     tasks: {
@@ -1414,6 +1417,16 @@ export interface FooterSelect<T extends boolean = true> {
   updatedAt?: T;
   createdAt?: T;
   globalType?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "collections_widget".
+ */
+export interface CollectionsWidget {
+  data?: {
+    [k: string]: unknown;
+  };
+  width: 'full';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
